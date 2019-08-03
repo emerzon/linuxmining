c671426bbb316fff18485fce54e54967ad5d968b
Emerson Gomes <emerson.gomes@gmail.com>
2019-08-03 22:51:52 +0200

    fix

--- cpu_conceal_xmrig.sh
+++ cpu_conceal_xmrig.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 screen -dmS cpu ./bin/xmrig -a cn/conceal -o $(< pools/conceal.pool) -u $(< wallets/conceal.address) -p x
--- nvidia_grin31_gminer.sh
+++ nvidia_grin31_gminer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ./nvidia_init.sh
-POOL=$(cat pools/grin.pool)
-screen -mS grin ./bin/miner --algo grin31 --server ${POOL%%:*} --port ${POOL#*:} --user $(cat wallets/grin.address)
+POOL=$(< pools/grin.pool)
+screen -mS grin ./bin/miner --algo grin31 --server ${POOL%%:*} --port ${POOL#*:} --user $(< wallets/grin.address)
