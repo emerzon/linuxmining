ce3a8e494ac5a3b3a5b8ecd2411fd83b3655b55e
Emerson Gomes <emerson.gomes@gmail.com>
2019-08-04 12:46:26 +0200

    add xmrig beta/conceal

--- cpu_conceal_xmrig.sh
+++ cpu_conceal_xmrig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -dmS cpu ./bin/xmrig -a cn/conceal -o $(< pools/conceal.pool) -u $(< wallets/conceal.address) -p x
+screen -dmS cpu ./bin/xmrig-conceal -a cn/conceal -o $(< pools/conceal.pool) -u $(< wallets/conceal.address) -p x
