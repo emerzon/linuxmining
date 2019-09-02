8691fef1f2fdca1eb030b90237864c5c06ea6f89
Emerson Gomes <emerson.gomes@gmail.com>
2019-09-02 07:12:20 +0200

    fix

--- nvidia_argon2d_cryptodredge.sh
+++ nvidia_argon2d_cryptodredge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./nvidia_init.sh
  
-screen -mS nvidia ./bin/CryptoDredge -a argon2d4096 -o $(< pools/XMY.pool) -u $(< wallets/BTC.address) -p x
+screen -mS nvidia ./bin/CryptoDredge -a argon2d4096 -o $(< pools/XMY.pool) -u $(< wallets/BTC.address),zap=XMY-argon2d -p x
