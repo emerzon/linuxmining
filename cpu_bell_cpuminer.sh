1251024284e5817d03d45395c720887840188e9f
Emerson Gomes <emerson.gomes@gmail.com>
2019-09-29 15:26:21 +0200

    updated cpuminer

--- cpu_bell_cpuminer.sh
+++ cpu_bell_cpuminer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -dmS cpu ./bin/cpuminer -t 16 --cpu-affinity 0xFFFF -a yespower -u $(< wallets/bell.address) -o stratum+tcp://$(< pools/bell.pool) -p x
+screen -dmS cpu ./bin/cpuminer -t 8 --cpu-affinity 0xFF -a yespower -u $(< wallets/bell.address) -o stratum+tcp://$(< pools/bell.pool) -p x
--- cpu_m7m_cpuminer.sh
+++ cpu_m7m_cpuminer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PAYOUT="LTC"
-screen -dmS cpu ./bin/cpuminer -t 16 --cpu-affinity 0x00000000FFFF0000 -a m7m -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/m7m.pool) -p c=${PAYOUT}
+screen -dmS cpu ./bin/cpuminer -t 31 -a m7m -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/m7m.pool) -p c=${PAYOUT}
