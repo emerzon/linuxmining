95aac1efdf18a1d7d299c8b272d91c4643f21492
Emerson Gomes <emerson.gomes@gmail.com>
2019-10-15 22:08:32 +0200

    no more solo

--- cpu_m7m_cpuminer.sh
+++ cpu_m7m_cpuminer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PAYOUT="LTC"
-screen -dmS cpu ./bin/cpuminer -a m7m -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/m7m.pool) -p m=solo,c=${PAYOUT}
+screen -dmS cpu ./bin/cpuminer -a m7m -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/m7m.pool) -p c=${PAYOUT}
