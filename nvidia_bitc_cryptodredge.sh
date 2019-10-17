243cef4faac04d5e1de253096d61e83201de40bf
Emerson Gomes <emerson.gomes@gmail.com>
2019-10-17 23:47:00 +0200

    updated x16rv2 pool

--- nvidia_bitc_cryptodredge.sh
+++ nvidia_bitc_cryptodredge.sh
@@ -2,4 +2,4 @@
 
 ./nvidia_init.sh
  
-screen -mS nvidia ./bin/CryptoDredge -a x16rv2 -o $(< pools/BITC.pool) -u $(< wallets/BTC.address) -p c=BTC
+screen -mS nvidia ./bin/CryptoDredge -a x16rv2 -o $(< pools/BITC.pool) -u $(< wallets/LTC.address) -p c=LTC,mc=BITC
