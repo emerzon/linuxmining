d4625d4bba338827dc31f44010df8f8942a01403
Emerson Gomes <emerson.gomes@gmail.com>
2019-10-20 20:28:42 +0200

    startup

--- nvidia_x16rv2_trex.sh
+++ nvidia_x16rv2_trex.sh
@@ -2,4 +2,4 @@
 
 ./nvidia_init.sh
  
-screen -mS nvidia ./bin/t-rex -a x16rv2 -o $(< pools/BITC.pool) -u $(< wallets/LTC.address) -p c=LTC
+screen -dmS nvidia ./bin/t-rex -a x16rv2 -o $(< pools/BITC.pool) -u $(< wallets/LTC.address) -p c=LTC
