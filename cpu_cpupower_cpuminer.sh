27a52649ec3183d66f08e8e9e66ac6a03d63c87b
Emerson Gomes <emerson.gomes@gmail.com>
2019-10-23 08:52:48 +0200

    added yespower

--- cpu_cpupower_cpuminer.sh
+++ cpu_cpupower_cpuminer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PAYOUT="LTC"
-screen -dmS cpu2 ./bin/cpuminer-cpupower-zen2 -t 10 -a cpupower -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/cpupower.pool) -p c=${PAYOUT}
+screen -dmS cpu2 ./bin/cpuminer-cpupower-zen2 -t 6 --cpu-affinity=0x555 -a cpupower -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/cpupower.pool) -p c=${PAYOUT}
