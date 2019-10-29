abbedea23908d114b912bf459960bd4ad4da1360
Emerson Gomes <egomes@sagan.chita.com.br>
2019-10-29 08:21:59 +0100

    fix

--- cpu_power2b_cpuminer.sh
+++ cpu_power2b_cpuminer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PAYOUT="LTC"
-screen -dmS cpu ./bin/cpuminer -t 6 --hash-meter -a power2b -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/power2b.pool) -p c=${PAYOUT}
+screen -dmS cpu2 ./bin/cpuminer -t 6 --hash-meter -a power2b -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/power2b.pool) -p c=${PAYOUT}
