79b0e731d78fbcb8e19478e87678942e0be9e381
Emerson Gomes <egomes@sagan.chita.com.br>
2019-11-18 22:52:30 +0100

    zen2 miners

--- cpu_yescryptr32_cpuminer.sh
+++ cpu_yescryptr32_cpuminer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PAYOUT="LTC"
-screen -dmS cpu2 ./bin/cpuminer -t 8 --hash-meter -a yescryptr32 -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/yescryptr32.pool) -p c=${PAYOUT}
+screen -dmS cpu2 ./bin/cpuminer-dyn-zen2 -t 6 --hash-meter -a yescryptr32 -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/yescryptr32.pool) -p c=${PAYOUT}
