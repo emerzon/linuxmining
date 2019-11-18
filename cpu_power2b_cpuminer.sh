03083940eef5dccab88678af0369b44517e14a7c
Emerson Gomes <emerson.gomes@gmail.com>
2019-11-18 23:45:21 +0100

    updated cpuminer

--- cpu_power2b_cpuminer.sh
+++ cpu_power2b_cpuminer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PAYOUT="LTC"
-screen -dmS cpu2 ./bin/cpuminer --cpu-affinity 0x00FFFF00 -t 16 --hash-meter -a power2b -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/power2b.pool) -p c=${PAYOUT}
+screen -dmS cpu2 ./bin/cpuminer --cpu-affinity 0x00FFFF00 -t 6 --hash-meter -a power2b -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/power2b.pool) -p c=${PAYOUT}
