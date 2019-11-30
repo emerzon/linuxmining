f70f858ede07e0fb19e1fb55a6fc55047cc50b1c
Emerson Gomes <emerson.gomes@gmail.com>
2019-11-30 18:52:56 +0100

    fix mon

--- cpu_loki_xmrig.sh
+++ cpu_loki_xmrig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -dmS cpu ./bin/xmrig-zen2 -a rx/loki -o $(< pools/LOKI.pool) -u $(< wallets/LOKI.address) -p x
+screen -dmS cpu ./bin/xmrig -a rx/loki -o $(< pools/LOKI.pool) -u $(< wallets/LOKI.address) -p x
