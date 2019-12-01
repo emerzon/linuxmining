7aaba8e0be2c77edf07afae0e3cccce14e72ee85
Emerson Gomes <emerson.gomes@gmail.com>
2019-12-01 19:54:36 +0100

    xmrig i5

--- cpu_monero_xmrig.sh
+++ cpu_monero_xmrig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -dmS cpu ./bin/xmrig-zen2 --coin monero -o $(< pools/XMR.pool) -u emerzon.vega -p x
+screen -dmS cpu ./bin/xmrig-i5 --coin monero -o $(< pools/XMR.pool) -u emerzon.vega -p x
