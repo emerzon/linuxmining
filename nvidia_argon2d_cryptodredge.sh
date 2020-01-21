97286841ea03548e93beacccad13b9597b6bde5d
Emerson Gomes <emerson.gomes@gmail.com>
2020-01-21 07:52:40 +0100

    update stuff

--- nvidia_argon2d_cryptodredge.sh
+++ nvidia_argon2d_cryptodredge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./nvidia_init.sh
  
-screen -mS nvidia ./bin/CryptoDredge -d 1 -a argon2d4096 -o $(< pools/XMY.pool) -u $(< wallets/BTC.address) -p c=BTX,zap=XMY-argon2d
+screen -mS nvidia ./bin/CryptoDredge -a argon2d4096 -o $(< pools/XMY.pool) -u $(< wallets/LTC.address) -p c=LTC
--- nvidia_init.sh
+++ nvidia_init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo nvidia-smi -pm 1
-sudo nvidia-smi -i 0 -pl 140
-sudo nvidia-smi -i 1 -pl 200
+sudo nvidia-smi -i 0 -pl 200
