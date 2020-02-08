4b41b2289d05d33894122b5e06d1995628a1484f
Emerson Gomes <egomes@sagan.chita.com.br>
2020-02-08 15:57:01 +0100

    update ct31

--- nvidia_grin31_bminer.sh
+++ nvidia_grin31_bminer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Change the following address to your GRIN addr.
-ADDRESS=$(< wallets/grin.address)
+ADDRESS=$(< wallets/NH2.address)
 USERNAME=$ADDRESS.$(hostname)
-POOL=$(< pools/grin.pool)
+POOL=$(< pools/ct31.pool)
 SCHEME=cuckatoo31
 PWD=x
 
 ./nvidia_init.sh
 
-screen -mS nvidia ./bin/bminer -fast -nofee -devices 0 -uri $SCHEME://$USERNAME:$PWD@$POOL -api 0.0.0.0:1880
+screen -mS nvidia ./bin/bminer -fast -nofee -uri $SCHEME://$USERNAME:$PWD@$POOL -api 0.0.0.0:1880
