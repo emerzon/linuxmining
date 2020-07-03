fe9d46a1573fbde71681403a204c611d6c76d20e
Emerson Gomes <emerson.gomes@gmail.com>
2020-07-03 22:52:47 +0200

    fix

--- cpu_yescryptr8g_cpuminer.sh
+++ cpu_yescryptr8g_cpuminer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PAYOUT="LTC"
-screen -dmS cpu cpuminer --hash-meter -a yescryptr8g -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/yescryptr8h.pool) -p c=${PAYOUT}
+screen -dmS cpu bin/cpuminer-opt-$(cat /proc/cpuinfo | grep flags | uniq | md5sum | cut -b 1-8) --hash-meter -a yescryptr8g -u $(< wallets/${PAYOUT}.address) -o stratum+tcp://$(< pools/yescryptr8g.pool) -p c=${PAYOUT}
