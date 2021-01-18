06047659fc66b7235a4be2ec4fb182738729ee55
Emerson Gomes <emerson.gomes@gmail.com>
2021-01-18 23:20:44 +0100

    new wallets

--- cpu_monero_xmrig.sh
+++ cpu_monero_xmrig.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -dmS cpu sudo ./bin/xmrig --randomx-1gb-pages --coin monero -o $(< pools/XMR.pool) -u $(< wallets/XMR.address) -p x
+screen -dmS cpu sudo ./bin/xmrig-$(cat /proc/cpuinfo | grep flags | uniq | md5sum | cut -b 1-8)  --randomx-1gb-pages --coin monero -o $(< pools/XMR.pool) -u $(< wallets/XMR.address) -p x
--- nvidia_eth_lolminer.sh
+++ nvidia_eth_lolminer.sh
@@ -2,4 +2,4 @@
 
 ./nvidia_init.sh
 screen -S ethpill -dm sudo bin/OhGodAnETHlargementPill-r2
-screen -S nvidia  -dm ./bin/lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 --user 32VSBG6C8crzyDYhFiEHQVzteL7MDVNukM.$(hostname) 
+screen -S nvidia  -dm ./bin/lolMiner --algo ETHASH --pool stratum+tcp://$(< pools/ETH.pool) --user $(< wallets/ETH.wallet).$(hostname) 
