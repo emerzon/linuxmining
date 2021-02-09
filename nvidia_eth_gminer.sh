2b0ea12ea550ed4507c0098ce8f6841294342bb5
Emerson Gomes <emerson.gomes@gmail.com>
2021-02-09 08:16:23 +0100

    added gminer

---
+++ nvidia_eth_gminer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#./nvidia_init.sh
+screen -S ethpill -dm sudo bin/OhGodAnETHlargementPill-r2
+screen -S nvidia -dm ./bin/gminer --algo ethash --server $(< pools/ETH.pool) -u $(< wallets/ETH.wallet).$(hostname) -p x
